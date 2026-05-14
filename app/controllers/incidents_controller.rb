2a3e6359ed57c3d9a1a48cc350e7f432b3196218
nmon <nisidakeizi@gmail.com>
2026-05-15 05:02:00 +0900

    fix:ページネーション対応

--- app/controllers/incidents_controller.rb
+++ app/controllers/incidents_controller.rb
@@ -1,4 +1,6 @@
 class IncidentsController < ApplicationController
+  before_action :authenticate_user!
   def index
+    @incidents = current_user.incidents.order(created_at: :desc).page(params[:page]).per(5)
   end
 end
