807d5e8ee3beb4ba3e8fc9e89b19f3de22e8ab50
nmon <nisidakeizi@gmail.com>
2026-05-17 12:38:43 +0900

    feat:時系列テーブルマイグレーションファイル作成

---
+++ db/migrate/20260517033027_create_incident_times.rb
@@ -0,0 +1,15 @@
+class CreateIncidentTimes < ActiveRecord::Migration[7.2]
+  def change
+    create_table :incident_times do |t|
+      t.integer :year
+      t.integer :month
+      t.integer :date
+      t.integer :hour
+      t.integer :minuts
+      t.integer :second
+      t.string :body
+      t.references :incident, foreign_key: true
+      t.timestamps
+    end
+  end
+end
