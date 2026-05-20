8470d9d6c159bc34973016f738c8c924e9c933ad
nmon <nisidakeizi@gmail.com>
2026-05-20 17:44:46 +0900

    feat:gemfileスキーマファイルpush

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_05_17_033027) do
+ActiveRecord::Schema[7.2].define(version: 2026_05_20_074644) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,7 +19,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_05_17_033027) do
     t.integer "month"
     t.integer "date"
     t.integer "hour"
-    t.integer "minuts"
+    t.integer "minute"
     t.integer "second"
     t.string "body", null: false
     t.integer "position"
