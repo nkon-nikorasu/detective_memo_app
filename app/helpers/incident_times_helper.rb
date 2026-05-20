5daed5546f6e5ddf315f9b6fbbc57b5ab69a303e
nmon <nisidakeizi@gmail.com>
2026-05-20 17:05:49 +0900

    feat:時間表示編集

--- app/helpers/incident_times_helper.rb
+++ app/helpers/incident_times_helper.rb
@@ -1,2 +1,45 @@
 module IncidentTimesHelper
+  def yeardeco(time)
+    if time.year.nil?
+      ""
+    else
+      "#{time.year}年"
+    end
+  end
+  def monthdeco(time)
+    if time.month.nil?
+      ""
+    else
+      "#{time.month}月"
+    end
+  end
+  def datedeco(time)
+    if time.date.nil?
+      ""
+    else
+      "#{time.date}日"
+    end
+  end
+  def hourdeco(time)
+    return "" if time.hour.nil?
+    if time.minute.nil? && time.second.nil?
+      "#{time.hour}時頃"
+    else
+      "#{time.hour}時"
+    end
+  end
+  def minutedeco(time)
+    if time.minute.nil?
+      ""
+    else
+      "#{time.minute}分"
+    end
+  end
+  def seconddeco(time)
+    if time.second.nil?
+      ""
+    else
+      "#{time.second}秒"
+    end
+  end
 end
