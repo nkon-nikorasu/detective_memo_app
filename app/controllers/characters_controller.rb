fd8d4d51833d3e83cedb7d9aad0ab416e6d9458e
nmon <nisidakeizi@gmail.com>
2026-05-26 19:25:09 +0900

    feat:edit,updateアクション定義

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,14 +1,33 @@
 class CharactersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_character, only: [:edit, :update]
   def create
     @incident = current_user.incidents.find(params[:incident_id])
     @character = @incident.characters.build(character_params)
     @character.save
   end
 
+  def edit
+  end
+
+  def update
+    if @character.update(character_params)
+      redirect_to edit_incident_path(@incident, anchor: "character-id-#{@character.id}" ),
+      notice: t('defaults.flash_message.updated', item: Character.model_name.human)
+    else
+      flash.now[:alert] = t('defaults.flash_message.not_updated', item: Character.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def character_params
     params.require(:character).permit(:name, :gender, :age, :role, :body)
+  end
+
+  def set_character
+    @incident = current_user.incidents.find(params[:incident_id])
+    @character = @incident.characters.find(params[:id])
   end
 end
