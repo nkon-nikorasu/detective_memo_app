6ab9deb53ce21e0ad1f9149ad97fc83429918b37
nmon <nisidakeizi@gmail.com>
2026-06-02 04:12:28 +0900

    feat:createアクションとストロングパラメーター定義

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,16 @@
 class MemosController < ApplicationController
+  before_action :authenticate_user!
+  #before_action :set_character, only: [:edit, :update, :destroy]
+
+  def create
+    @incident = current_user.incidents.find(params[:incident_id])
+    @memo = @incident.memos.build(memo_params)
+    @memo.save
+  end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:tag, :body)
+  end
 end
