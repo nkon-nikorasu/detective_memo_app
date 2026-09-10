cf8dba41ae810860f76c6bcea2b6158e33f1fcde
nmon <nisidakeizi@gmail.com>
2026-09-10 14:55:41 +0900

    feat:factories作成

--- spec/factories/characters.rb
+++ spec/factories/characters.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :character do
-    
+    name { "田中太郎" }
+    age { 30 }
+    role { "容疑者" }
+    body { "事件の容疑者の一人" }
+    gender { :man }
+
+    association :incident
   end
 end
--- spec/factories/memos.rb
+++ spec/factories/memos.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :memo do
-    
+    body { "犯人についての推理メモ" }
+    tag { :deduction }
+    association :incident
   end
 end
