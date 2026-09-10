5ae9376c96eb0a15b3f752672a79a2fdd93df723
nmon <nisidakeizi@gmail.com>
2026-09-10 14:56:44 +0900

    feat:ヘルパーファイル内編集

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -39,7 +39,8 @@ RSpec.configure do |config|
   config.fixture_paths = [
     Rails.root.join('spec/fixtures')
   ]
-
+  config.include FactoryBot::Syntax::Methods
+  config.include Warden::Test::Helpers, type: :system
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
