4dce5f144ebbf253351183395f6b34351bb520b2
nmon <nisidakeizi@gmail.com>
2026-09-10 17:34:23 +0900

    fix:ダブルクォートに修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
-  def page_title(title = '')
-    base_title = 'DetectiveMemo'
+  def page_title(title = "")
+    base_title = "DetectiveMemo"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
 
